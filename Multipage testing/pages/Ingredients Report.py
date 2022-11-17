fb1955b8523a2ab5aa4baa9cc9d58b248b1d8616
Yian-anan <115465643+Yian-anan@users.noreply.github.com>
2022-11-17 18:02:06 +0100

    update 1

--- Multipage testing/pages/Ingredients Report.py
+++ Multipage testing/pages/Ingredients Report.py
@@ -14,11 +14,11 @@ LOCAL_TEST_URL = 'http://127.0.0.1:5000/recipeCO2'
 GCP_BACKEND_URL = 'XXX.XXX.XXX.XXX'
 
 
-# register_page(__name__)
-app = dash.Dash(__name__, assets_folder="assets")
+register_page(__name__)
+# app = dash.Dash(__name__, assets_folder="assets")
 
-app.layout = html.Div([
-# layout = html.Div([
+# app.layout = html.Div([
+layout = html.Div([
          html.H1("🌿 GREEN RECIPE 🌿",style={"text-align": "center","marginTop":100,"marginBottom":20, "font-size":60}),
          html.H2('CO2 Emssion of ingredients',style={"text-align": "center"}),
          dcc.Graph('id=ingrd_cat_fig1',style={'display': 'inline-block'}) 
@@ -28,7 +28,7 @@ app.layout = html.Div([
 def update_result(n_clicks, value):
 
      backendURL = LOCAL_TEST_URL
-     response = requests.get(url = backendURL,  params={'cat':catergory})
+     response = requests.get(url = backendURL,  params={'cat':catergory}) #adjust later 
      ingrd=[]
      co2=[]
      if (response.status_code != 204 and
@@ -59,5 +59,5 @@ def parsingCategoryCO2(response_json):
 
 
 ## THIS IS FOR SINGLE PAGE TESTING  
-if __name__ == '__main__':
-    app.run(host = '127.0.0.1',port = 8093,debug = True)
+# if __name__ == '__main__':
+#     app.run(host = '127.0.0.1',port = 8093,debug = True)
