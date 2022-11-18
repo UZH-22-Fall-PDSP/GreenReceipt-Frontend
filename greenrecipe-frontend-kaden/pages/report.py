32b74b6cdb18f68cc9962f68b44a69ef9fee7c84
Zzzzmq <91421613+Zzzzmq@users.noreply.github.com>
2022-11-18 13:53:48 +0100

    2

--- greenrecipe-frontend-kaden/pages/report.py
+++ greenrecipe-frontend-kaden/pages/report.py
@@ -6,37 +6,37 @@ import pandas as pd
 
 dash.register_page(__name__)
 
-LOCAL_TEST_URL = 'http://127.0.0.1:5000'
-GCP_BACKEND_URL = 'XXX.XXX.XXX.XXX'
+# LOCAL_TEST_URL = 'http://127.0.0.1:5000'
+# GCP_BACKEND_URL = 'XXX.XXX.XXX.XXX'
 
-def getCatIngrdList(category):
-    backendURL = LOCAL_TEST_URL + '/ingrdcat'
-    response = requests.get(url = backendURL,  params={'cat': category})
-    if (response.status_code != 204 and
-        response.headers["content-type"].strip().startswith("application/json")):
-        try:
-            response_json = response.json()
-        except ValueError:
-            True
-    return response_json
+# def getCatIngrdList(category):
+#     backendURL = LOCAL_TEST_URL + '/ingrdcat'
+#     response = requests.get(url = backendURL,  params={'cat': category})
+#     if (response.status_code != 204 and
+#         response.headers["content-type"].strip().startswith("application/json")):
+#         try:
+#             response_json = response.json()
+#         except ValueError:
+#             True
+#     return response_json
 
-vegetables = {'name':'Vegetables','data':getCatIngrdList('Vegetables')}
-cat_vege_fig = px.pie(pd.DataFrame(vegetables['data']).sort_values('co2',ascending=False)[:10], names='ingredient', values='co2', hole=.4)
+# vegetables = {'name':'Vegetables','data':getCatIngrdList('Vegetables')}
+# cat_vege_fig = px.pie(pd.DataFrame(vegetables['data']).sort_values('co2',ascending=False)[:10], names='ingredient', values='co2', hole=.4)
 
-fruits = {'name':'Fruits','data':getCatIngrdList('Fruits')}
-cat_fruits_fig = px.pie(pd.DataFrame(fruits['data']).sort_values('co2',ascending=False)[:10], names='ingredient', values='co2', hole=.4)
+# fruits = {'name':'Fruits','data':getCatIngrdList('Fruits')}
+# cat_fruits_fig = px.pie(pd.DataFrame(fruits['data']).sort_values('co2',ascending=False)[:10], names='ingredient', values='co2', hole=.4)
 
-grains = {'name':'Grains, Beans and Nuts','data':getCatIngrdList('Grains, Beans and Nuts')}
-cat_grains_fig = px.pie(pd.DataFrame(grains['data']).sort_values('co2',ascending=False)[:10], names='ingredient', values='co2', hole=.4)
+# grains = {'name':'Grains, Beans and Nuts','data':getCatIngrdList('Grains, Beans and Nuts')}
+# cat_grains_fig = px.pie(pd.DataFrame(grains['data']).sort_values('co2',ascending=False)[:10], names='ingredient', values='co2', hole=.4)
 
-meat = {'name':'Meat and Poultry','data':getCatIngrdList('Meat and Poultry')}
-cat_meat_fig = px.pie(pd.DataFrame(meat['data']).sort_values('co2',ascending=False)[:10], names='ingredient', values='co2', hole=.4)
+# meat = {'name':'Meat and Poultry','data':getCatIngrdList('Meat and Poultry')}
+# cat_meat_fig = px.pie(pd.DataFrame(meat['data']).sort_values('co2',ascending=False)[:10], names='ingredient', values='co2', hole=.4)
 
-fish = {'name':'Fish and Seafood','data':getCatIngrdList('Fish and Seafood')}
-cat_fish_fig = px.pie(pd.DataFrame(fish['data']).sort_values('co2',ascending=False)[:10], names='ingredient', values='co2', hole=.4)
+# fish = {'name':'Fish and Seafood','data':getCatIngrdList('Fish and Seafood')}
+# cat_fish_fig = px.pie(pd.DataFrame(fish['data']).sort_values('co2',ascending=False)[:10], names='ingredient', values='co2', hole=.4)
 
-dairy = {'name':'Dairy Foods','data':getCatIngrdList('Dairy Foods')}
-cat_dairy_fig = px.pie(pd.DataFrame(dairy['data']).sort_values('co2',ascending=False)[:10], names='ingredient', values='co2', hole=.4)
+# dairy = {'name':'Dairy Foods','data':getCatIngrdList('Dairy Foods')}
+# cat_dairy_fig = px.pie(pd.DataFrame(dairy['data']).sort_values('co2',ascending=False)[:10], names='ingredient', values='co2', hole=.4)
 
 layout = html.Div([
                 html.Div([
@@ -51,12 +51,12 @@ layout = html.Div([
                                     # margin for the district
                                     "margin-top":"-10px"}
                         ),                
-                html.Div([
+                # html.Div([
                         ## [SUB-COMPONENT] Category Ingredient result - Category Graphs
-                        dcc.Graph(id='cat1_ingrd_graph', figure = cat_dairy_fig),
-                        dcc.Graph(id='cat1_ingrd_graph', figure = cat_fish_fig),
-                        dcc.Graph(id='cat1_ingrd_graph', figure = cat_meat_fig),
-                        ],style={'display': 'flex', 'flex-direction': 'row'}),
+                        # dcc.Graph(id='cat1_ingrd_graph', figure = cat_dairy_fig),
+                        # dcc.Graph(id='cat1_ingrd_graph', figure = cat_fish_fig),
+                        # dcc.Graph(id='cat1_ingrd_graph', figure = cat_meat_fig),
+                        # ],style={'display': 'flex', 'flex-direction': 'row'}),
                 html.Br(), html.Br(),
                 ])
 
